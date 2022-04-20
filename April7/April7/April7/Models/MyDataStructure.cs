c3a2181ce53be9a2d958260d27b3c52b0b06118a
Hatamli <kamilahat@code.edu.az>
2022-04-20 16:17:17 +0400

    task finally complated

---
+++ April7/April7/April7/Models/MyDataStructure.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace April7.Models
+{
+    public class MyDataStructure<T>
+    {
+        T[] array = null;
+
+        public T[] GetArray { get { return array; } }
+        public int Length { get { return array.Length; } }
+        public MyDataStructure()
+        {
+            array = new T[1];
+
+        }
+        public void Add(T item)
+        {
+            array[array.Length - 1] = item;
+            myInitialize(ref array, array.Length + 1);
+        }
+        private void myInitialize(ref T[] array, int newLength)
+        {
+            if (array == null)
+            {
+                array = new T[newLength];
+            }
+            else
+            {
+                T[] tempArr = new T[newLength];
+
+                for (int i = 0; i < newLength; i++)
+                    if (i < array.Length)
+                    {
+                        tempArr[i] = array[i];
+                    }
+                    else
+                    {
+                        tempArr[i] = default(T);
+                    }
+                array = new T[newLength];
+
+                for (int i = 0; i < newLength; i++)
+                {
+                    array[i] = tempArr[i];
+                }
+            }
+        }
+    }
+}
---
+++ April7/April7/April7/Program.cs
@@ -0,0 +1,23 @@
+﻿using April7.Models;
+using System;
+
+namespace April7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDataStructure<string> myDataStructure = new MyDataStructure<string>();
+
+            myDataStructure.Add("Kamil");
+            myDataStructure.Add("Hatamli");
+
+            myDataStructure.Add("Hatam");
+
+            for (int i = 0; i < myDataStructure.Length; i++)
+            {
+                Console.WriteLine(myDataStructure.GetArray[i]);
+            }
+        }
+    }
+}
